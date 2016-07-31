a71f7b050d77fe206639165a683a5433eb407a34
sunnysahu34 <sunny.sahu@ten24web.com>
2016-07-31 16:06:29 -0400

    removed comments from ExtractFunctionTool

--- src/codecoverage/ExtractFunctionTool.java
+++ src/codecoverage/ExtractFunctionTool.java
@@ -7,14 +7,7 @@ import java.util.*;
 public class ExtractFunctionTool extends CFSCRIPTParserBaseListener {
 	List<String> functionNames = new ArrayList<String>();
    int totalNumberOfFunction=0;
-//private static ExtractFunctionTool singletonObject;
-//private ExtractFunctionTool(){}
-//public static synchronized ExtractFunctionTool getSingletonObject(){
-//	if(singletonObject==null){
-//		singletonObject= new ExtractFunctionTool();
-//	}
-//	return singletonObject;
-//}
+
 
 	@Override
 	public void enterFunctionDeclaration(CFSCRIPTParser.FunctionDeclarationContext ctx) {
@@ -22,21 +15,7 @@ public class ExtractFunctionTool extends CFSCRIPTParserBaseListener {
 		totalNumberOfFunction ++;
 	
 		functionNames.add(ctx.identifier().getText());
-	//	System.out.println("this is the value number of functions="+totalNumberOfFunction);
-		
-	/*	for (int i=0;i < functionNames.size();i++)
-		{
-			count ++;
-		}
-		
-		System.out.println("total number of functions="+count);
 	
-	}
-	public static int counter()
-	{
-		return count;
-		
-	}*/
 	
 	}
 
